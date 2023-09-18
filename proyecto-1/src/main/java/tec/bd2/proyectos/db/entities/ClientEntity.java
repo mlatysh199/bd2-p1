357b4ba8b9ecd0806d8d9b86697bb5d9355c20df
Antonio Fernandez <antonioefernandezg@gmail.com>
2023-09-17 19:53:59 -0600

    Client y Log

--- proyecto-1/src/main/java/tec/bd2/proyectos/db/entities/ClientEntity.java
+++ proyecto-1/src/main/java/tec/bd2/proyectos/db/entities/ClientEntity.java
@@ -1,5 +1,69 @@
 package tec.bd2.proyectos.db.entities;
 
 public class ClientEntity implements Entity {
+
+    private int id;
+    private String nombre;
+    private String fechaUltimaCompra;
+    private String correo;
+    private String direccion;
+    private int cantidadCompras;
+
+    public ClientEntity(int id, String nombre, String fechaUltimaCompra, String correo, String direccion, int cantidadCompras) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fechaUltimaCompra = fechaUltimaCompra;
+        this.correo = correo;
+        this.direccion = direccion;
+        this.cantidadCompras = cantidadCompras;
+    }
+
+    public ClientEntity (String nombre, String fechaUltimaCompra, String correo, String direccion, int cantidadCompras) {
+        this.nombre = nombre;
+        this.fechaUltimaCompra = fechaUltimaCompra;
+        this.correo = correo;
+        this.direccion = direccion;
+        this.cantidadCompras = cantidadCompras;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getFechaUltimaCompra() {
+        return fechaUltimaCompra;
+    }
+    public String getCorreo() {
+        return correo;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public int getCantidadCompras() {
+        return cantidadCompras;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setFechaUltimaCompra(String fechaUltimaCompra) {
+        this.fechaUltimaCompra = fechaUltimaCompra;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public void setCantidadCompras(int cantidadCompras) {
+        this.cantidadCompras = cantidadCompras;
+    }
+
+
     
 }
--- proyecto-1/src/main/java/tec/bd2/proyectos/db/repository/LogRepository.java
+++ proyecto-1/src/main/java/tec/bd2/proyectos/db/repository/LogRepository.java
@@ -1,39 +1,63 @@
 package tec.bd2.proyectos.db.repository;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import tec.bd2.proyectos.db.entities.ClientEntity;
 import tec.bd2.proyectos.db.entities.LogEntity;
 
 public class LogRepository implements BaseRepository<LogEntity> {
+     private Connection conn;
 
+    public LogRepository(Connection conn) {
+        this.conn = conn;
+    }
     @Override
     public void save(LogEntity entity) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+       
     }
 
     @Override
     public void update(LogEntity entity) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        
     }
 
     @Override
     public void delete(int id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+  
     }
 
     @Override
-    public LogEntity findById(int id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    public LogEntity findById(int id) throws SQLException {
+        CallableStatement cstmt = conn.prepareCall("{? = call paquete_select.obtener_bitacora_producto(?)}");
+        cstmt.registerOutParameter(1, oracle.jdbc.OracleTypes.CURSOR);
+        cstmt.setInt(2, id);
+        cstmt.execute();
+
+        ResultSet rs = ((oracle.jdbc.OracleCallableStatement)cstmt).getCursor(1);
+        rs.next();
+        return new LogEntity(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4));
     }
 
     @Override
-    public List<LogEntity> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    public List<LogEntity> findAll() throws SQLException {
+        CallableStatement cstmt = conn.prepareCall("{? = call paquete_select.obtener_bitacora_productos()}");
+        cstmt.registerOutParameter(1, oracle.jdbc.OracleTypes.CURSOR);
+
+        cstmt.execute();
+
+        // process the cursor returned by the stored procedure into a ResultSet
+        ResultSet rs = ((oracle.jdbc.OracleCallableStatement)cstmt).getCursor(1);
+        List<LogEntity> iterable = new ArrayList<>();
+        while(rs.next()) {
+            iterable.add(new LogEntity(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4)));
+        }
+        return iterable;
     }
+    
     
 }
